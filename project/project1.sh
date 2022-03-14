bad40dded17467d126cf91229e3d3107880be0b3
Matt Butrovich <mbutrovich@gmail.com>
2022-03-14 12:24:04 -0400

    sudo some stuff for maximum damage.

--- project/project1.sh
+++ project/project1.sh
@@ -291,7 +291,7 @@ main() {
 
   # TODO(Matt): Verify that these paths are correct.
   # Make a copy of the postgresql.conf file.
-  cp /etc/postgresql/14/main/postgresql.conf /etc/postgresql/14/main/postgresql.conf.old
+  sudo bash -c "cp /etc/postgresql/14/main/postgresql.conf /etc/postgresql/14/main/postgresql.conf.old"
 
   for benchmark_spec in "${BENCHMARKS[@]}"; do
     while IFS=',' read -r benchmark workload_csv; do
@@ -300,9 +300,9 @@ main() {
 
       # TODO(Matt): Verify that these paths are correct.
       # Remove the auto.conf that might have existed from prior student runs.
-      rm -rf /var/lib/postgresql/14/main/postgresql.auto.conf
+      sudo bash -c "rm -rf /var/lib/postgresql/14/main/postgresql.auto.conf"
       # Restore the original postgresql.conf file.
-      cp /etc/postgresql/14/main/postgresql.conf.old /etc/postgresql/14/main/postgresql.conf
+      sudo bash -c "cp /etc/postgresql/14/main/postgresql.conf.old /etc/postgresql/14/main/postgresql.conf"
 
       # Create the project database.
       _setup_database
@@ -332,9 +332,9 @@ main() {
       for student in "${STUDENTS[@]}"; do
         # TODO(Matt): Verify that these paths are correct.
         # Remove the auto.conf that might have been created by a prior student.
-        rm -rf /var/lib/postgresql/14/main/postgresql.auto.conf
+        sudo bash -c "rm -rf /var/lib/postgresql/14/main/postgresql.auto.conf"
         # Restore the original postgresql.conf file.
-        cp /etc/postgresql/14/main/postgresql.conf.old /etc/postgresql/14/main/postgresql.conf
+        sudo bash -c "cp /etc/postgresql/14/main/postgresql.conf.old /etc/postgresql/14/main/postgresql.conf"
 
         while IFS=',' read -r git_url andrew_id; do
           student_submission_path="${student_submission_folder}/${andrew_id}"
